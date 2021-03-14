6a86f039067874277e8fe656f6348f8476a7fbd4
Lucas Saporetti <lucassaporetti@gmail.com>
2021-03-14 18:37:30 -0300

    backend deleted

--- frogs_task_project/frogs_task_api_back/api/core/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Task
-
-
-class TaskSerializer(ModelSerializer):
-
-    class Meta:
-        model = Task
-        fields = ['uuid', 'status', 'name', 'date', 'time', 'task_type', 'priority']
--- frogs_task_project/frogs_task_api_back/api/frogs_api_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-clear
-sudo kill -9 $(sudo lsof -t -i:8000)
-./manage.py runserver
-clear
